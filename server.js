9a80c6b0b09bf1cc7acf43d02a37061e2d483cd6
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-05-06 00:04:33 +0930

    connected mongoDb

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
+// Load env variables
 dotenv.config({ path: './config/config.env' });
 
+// Connect to MongoDB
+connectDB();
+
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send({
+    success: true,
+    name: 'nash',
+  });
+});
 
 const PORT = process.env.PORT || 8000;
 
